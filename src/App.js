13240e0479be19dffb2773cfe87978bef9c1a1c4
Suju Tan <sujuktan@gmail.com>
2023-03-07 18:30:25 -0500

    added the robot card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
+import Card from "./Card";
+
+//css
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Card />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div>
+      <img src="https://robohash.org/test/" alt="a robot" />
+      <div>
+        <h2>This is a robot</h2>
+        <p>Email@gmail.com</p>
+      </div>
+    </div>
+  );
+}
