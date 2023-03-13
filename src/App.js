7deaa9b2b738ad26b6b490d0272e68ad3853f8ba
Suju Tan <sujuktan@gmail.com>
2023-03-13 11:38:01 -0400

    use the robots fake data from jsonplaceholder.com

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 
 import "tachyons";
 
@@ -11,17 +10,31 @@ import "./App.css";
 
 function App() {
   const [searchField, setSearchField] = useState("");
+  const [robots, setRobots] = useState([]);
+
+  //fetch the fake data from jsonplaceholder
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => setRobots(json));
+  }, []);
+
+  //handle the searchbox's value changed
   const handleSearchChange = (searchValue) => {
     setSearchField(searchValue);
   };
 
-  return (
-    <div className="tc">
-      <h1 className="f1">Robot Friends</h1>
-      <SearchBox handleSearchChange={handleSearchChange} />
-      <CardList robots={robots} searchField={searchField} />
-    </div>
-  );
+  if (robots.length === 0) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <div className="tc">
+        <h1 className="f1">Robot Friends</h1>
+        <SearchBox handleSearchChange={handleSearchChange} />
+        <CardList robots={robots} searchField={searchField} />
+      </div>
+    );
+  }
 }
 
 export default App;
